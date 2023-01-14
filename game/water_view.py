0d915d8262b0de5cd66925aa83866e0d5a54f91d
Wooniety <wooniety@gmail.com>
2023-01-15 04:32:40 +0800

    drink water

--- game/water_view.py
+++ game/water_view.py
@@ -4,7 +4,20 @@ from kivy.uix.button import Button
 class WaterView(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        pass
+        self.water_drank = 0
+
+    def drink_cup(self):
+        self.water_drank += 250
+
+    def remove_cup(self):
+        self.water_drank -= 250
+
+    def drink_bottle(self):
+        self.water_drank += 750
+
+    def remove_bottle(self):
+        self.water_drank -= 750
+    
 
 
 
