827e2b1c3177b2565a6c2704f51451c5ab1a5550
xTakoyaki <beliviankonglien@gmail.com>
2023-01-14 23:37:55 +0800

    Merge branch 'main' of https://github.com/Wooniety/Treasure-Hacks
    attempt to fix merge conflict

--- game/shop_view.py
+++ game/shop_view.py
@@ -1,9 +1,10 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.graphics import Color, Ellipse
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
 
 class ShopView(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        with self.canvas:
-            Color(1, 1, 0)
-            Ellipse(pos=(200,200), size=(100,100))
+        pass
+
+
+
