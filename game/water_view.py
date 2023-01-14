f5d34746a9c6a2cacea30bbdd11b61cf2e3bf13f
xTakoyaki <beliviankonglien@gmail.com>
2023-01-14 22:47:22 +0800

    adding placeholder images

---
+++ game/water_view.py
@@ -0,0 +1,9 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+
+class WaterView(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        
