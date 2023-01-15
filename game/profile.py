0117439332346a227bc65c37f54ea70515c3fbdf
luksz <tanpinyee@gmail.com>
2023-01-15 14:29:34 +0800

    hi

--- game/profile.py
+++ game/profile.py
@@ -1,9 +1,10 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
 
 class Profile(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
 
 
