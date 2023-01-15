fbeab94de71b8a31d09e03653421fc89f5e1c3ed
luksz <tanpinyee@gmail.com>
2023-01-15 14:40:49 +0800

    hello

--- game/profile.py
+++ game/profile.py
@@ -1,10 +1,14 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
-
+from kivy.uix.boxlayout import BoxLayout
 
 class Profile(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-
+        layout = BoxLayout(orientation='vertical')
+        self.textinput = TextInput(text='Hello World',size_hint=(1, .2), pos_hint={'center_x': .5, 'center_y': .5})
+        self.button = Button(text="Submit",size_hint=(1, .2))
+        layout.add_widget(self.textinput)
+        layout.add_widget(self.button)
+        self.add_widget(layout)
