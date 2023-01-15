44a80608ca1666f3a2db2a339de05ab7fd4d573a
Wooniety <wooniety@gmail.com>
2023-01-15 13:17:05 +0800

    water drinking function

--- game/water_view.py
+++ game/water_view.py
@@ -6,17 +6,28 @@ class WaterView(Screen):
         super().__init__(**kwargs)
         self.water_drank = 0
 
+    def update_label(self):
+        if self.water_drank < 0:
+            self.water_drank = 0
+        amt_str = f"Water Consumed: {self.water_drank}"
+        self.ids.water_drank.text = amt_str
+
     def drink_cup(self):
         self.water_drank += 250
+        self.update_label()
 
     def remove_cup(self):
         self.water_drank -= 250
+        self.update_label()
 
     def drink_bottle(self):
         self.water_drank += 750
+        self.update_label()
 
     def remove_bottle(self):
         self.water_drank -= 750
+        self.update_label()
+    
     
 
 
